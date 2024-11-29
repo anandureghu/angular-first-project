eda5ff4a6a02994bf877310ddd4fa20eda7f36bf
anandureghu <anandukr145@gmail.com>
2024-11-29 08:27:47 +0530

    added directives

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,17 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
   @Output() close = new EventEmitter<void>();
   enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onClose() {
     this.close.emit();
   }
+  onSubmit() {}
 }
