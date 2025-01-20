fce77b50381aaa3bdbbb7df7d740c95afe36b968
anandureghu <anandukr145@gmail.com>
2025-01-20 20:34:14 +0530

    project reset

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# CmpDeepDive
+# StudentManagement
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.0-next.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.11.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { StatusComponent } from './dashboard/status/status.component';
-import { TrafficComponent } from './dashboard/traffic/traffic.component';
-import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
-import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    StatusComponent,
-    TrafficComponent,
-    DashboardItemComponent,
-    TicketsComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: '../styles.css',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'student-management';
+}
